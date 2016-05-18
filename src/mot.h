fa39a09135dfeb0826f054e2b515dafe93742532
JeremBlain <blain.jerem@gmail.com>
2016-05-18 19:50:56 +0200

    Revert "My hangman game I made long time ago/ le jeu du pendu que j'ai fait il y a un petit moment."
    This reverts commit 88a8abaa57261f7f30135cd1eccf368e85fb7604.

--- src/mot.h
+++
@@ -1,9 +0,0 @@
-#ifndef MOT_H
-#define MOT_H
-
-/*
- * Toutes les fonctions autour du mot donné par le joueur se trouve ici
- *  Comme par exemple l'enregistrement du mot,  *
- */
-
-#endif // MOT_H
